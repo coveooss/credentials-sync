becd4382fceeb5e918c46bae1058742613fc3328
Julien Duchesne <jduchesne@coveo.com>
2019-07-17 19:54:16 -0400

    Exclude mock.go from coverage

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo "" > coverage.txt
 for d in $(go list ./... | grep -v vendor); do
     go test -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
+        cat profile.out | grep -v "_mock.go" >> coverage.txt
         rm profile.out
     fi
 done

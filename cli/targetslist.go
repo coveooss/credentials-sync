7cb2e217449b3a210b35ef678c8f95cf0e4296dd
Philippe Ballandras <pballandras@coveo.com>
2022-04-06 08:29:52 -0400

    initial fix idea

--- cli/targetslist.go
+++ cli/targetslist.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+
 	"github.com/coveooss/credentials-sync/logger"
 
 	"github.com/spf13/cobra"
@@ -10,12 +11,14 @@ import (
 var listTargetsCmd = &cobra.Command{
 	Use:   "list-targets",
 	Short: "Resolves and lists all configured targets",
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := configuration.Targets.ValidateConfiguration(); err != nil {
-			logger.Log.Fatalf("The targets section of the config file is invalid: %v", err)
+			logger.Log.Errorf("The targets section of the config file is invalid: %v", err)
+			return err
 		}
 		for _, target := range configuration.Targets.AllTargets() {
 			fmt.Println(target.ToString())
 		}
+		return nil
 	},
 }
